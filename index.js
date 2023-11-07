2e97d0f186356fb1e532bc99ec67c9865651ffc8
Ting-Yu Chen <7ingyu@gmail.com>
2023-11-06 17:30:27 -0800

    Added 404 route

--- index.js
+++ index.js
@@ -11,4 +11,8 @@ app.get('/', (req, res) => {
   res.send('Hellooooo')
 })
 
+app.get('*', (req, res) => {
+  res.status(404).send('<h1>404: Page not found</h1>')
+})
+
 app.listen(PORT, () => console.log('listening at', PORT))

d1353ed5f3ccd56b9ac5ed3d10755523a8d48d2e
Ting-Yu Chen <7ingyu@gmail.com>
2023-11-08 17:22:36 -0800

    added bootstrap and custom css

--- index.js
+++ index.js
@@ -9,8 +9,10 @@ const app = express()
 const PORT = process.env.PORT
 
 // MIDDLEWARE
+app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
 
 // ROUTES & CONTROLLERS
 app.get('/', (req, res) => {
@@ -23,4 +25,5 @@ app.get('*', (req, res) => {
   res.status(404).render('notfound')
 })
 
+// START SERVER
 app.listen(PORT, () => console.log('listening at', PORT))
--- views/default.jsx
+++ views/default.jsx
@@ -4,10 +4,21 @@ function Def ({ children }) {
   return (
     <html>
       <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>Title</title>
+        <link
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN"
+          crossOrigin="anonymous"
+        />
+        <link rel="stylesheet" href="/css/styles.css" />
       </head>
       <body>
         {children}
+        {/* <script
+          src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+          crossOrigin="anonymous"
+        /> */}
       </body>
     </html>
   )

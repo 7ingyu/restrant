f0a3d932fa057599e459ad95526a05b16cc4eb24
Ting-Yu Chen <7ingyu@gmail.com>
2023-11-08 17:00:30 -0800

    added default view layout

---
+++ views/default.jsx
@@ -0,0 +1,14 @@
+const React = require('react')
+
+function Def ({ children }) {
+  <html>
+    <head>
+      <title>Title</title>
+    </head>
+    <body>
+      {children}
+    </body>
+  </html>
+}
+
+module.exports = Def

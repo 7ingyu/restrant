60e6a78d18149de39a9688e4ed22c7af06d1c944
Ting-Yu Chen <7ingyu@gmail.com>
2023-11-11 07:29:24 -0800

    added footer

--- views/default.jsx
+++ views/default.jsx
@@ -15,15 +15,26 @@ function Def ({ children }) {
         <link rel="stylesheet" href="/styles.css" />
       </head>
       <body>
-        <Nav />
-        <div className="container">
-          {children}
+        <div id="app">
+          <div>
+            <Nav />
+            <div className="container">
+              {children}
+            </div>
+          </div>
+          <footer className="bg-secondary">
+            <div className="text-end text-white container py-3">
+              Copyright Me 2023{
+                new Date().getFullYear() > 2023 ? ` - ${new Date().getFullYear()}` : ''
+              }
+            </div>
+          </footer>
         </div>
-        {/* <script
-          src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
-          crossOrigin="anonymous"
-        /> */}
-        <script type="module" src="/scripts.js"></script>
+          {/* <script
+            src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+            crossOrigin="anonymous"
+          /> */}
+          <script type="module" src="/scripts.js"></script>
       </body>
     </html>
   )
